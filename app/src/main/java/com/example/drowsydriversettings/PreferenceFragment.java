24bdef895091b9bfd511f013ef775a4c39e02991
tri2401 <tri2401na@gmail.com>
2021-10-18 00:25:48 -0500

    Tool Bar with Menu and Settings

--- app/src/main/java/com/example/drowsydriversettings/PreferenceFragment.java
+++ app/src/main/java/com/example/drowsydriversettings/PreferenceFragment.java
@@ -10,8 +10,5 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class PreferenceFragment extends android.preference.PreferenceFragment {
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+
 }
