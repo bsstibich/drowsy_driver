f05e0a17a6a51ccb750014dd3ab1b69684727e74
tri2401 <tri2401na@gmail.com>
2021-10-18 00:19:59 -0500

    10/18/21 needs nav to preference

--- app/src/main/java/com/example/drowsydriversettings/MainSettings.java
+++ app/src/main/java/com/example/drowsydriversettings/MainSettings.java
@@ -3,10 +3,12 @@ package com.example.drowsydriversettings;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.app.FragmentTransaction;
 
 public class MainSettings extends AppCompatActivity {
 
@@ -15,14 +17,21 @@ public class MainSettings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_settings);
 
-        Toolbar toolbar = findViewById(R.id.toolbarSettings);
-        setSupportActionBar(toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbarSettings); //declaring and specifying toolbar
+        setSupportActionBar(toolbar); //setting tool bar
 
-        getSupportActionBar().setTitle("Settings");
+        getSupportActionBar().setTitle("Settings"); //title
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //adding back button
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onSupportNavigateUp() { //go back a page; possible through "getSupportActionBar().setDisplayHomeAsUpEnabled(true);"
+        finish();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { //creates tool bar drop down options
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return true;
---
+++ app/src/main/java/com/example/drowsydriversettings/PreferenceFragment.java
@@ -0,0 +1,17 @@
+package com.example.drowsydriversettings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class PreferenceFragment extends android.preference.PreferenceFragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
