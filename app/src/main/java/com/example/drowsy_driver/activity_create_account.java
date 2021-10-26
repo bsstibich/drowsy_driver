0389f493b2d0ebc0443b5666ce9917d08936570d
bsstibich <bsstibich@gmail.com>
2021-10-26 15:13:41 -0500

    make account creation functioning

--- app/src/main/java/com/example/drowsy_driver/activity_create_account.java
+++ app/src/main/java/com/example/drowsy_driver/activity_create_account.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class activity_create_account extends AppCompatActivity {
     private Button cancelButton;
@@ -13,6 +15,25 @@ public class activity_create_account extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        final EditText email = findViewById(R.id.email);
+        final EditText userName = findViewById(R.id.userName);
+        final EditText fullName = findViewById(R.id.fullName);
+        final EditText password = findViewById(R.id.password);
+        final EditText confirmPassword = findViewById(R.id.confirmPassword);
+        final EditText vehicleInfo = findViewById(R.id.vehicleInfo);
+        DAOAccount dao = new DAOAccount();
+
+        Button createAccount = findViewById(R.id.createAccount);
+        createAccount.setOnClickListener(v->{
+            Account acc = new Account(email.getText().toString(), userName.getText().toString(), fullName.getText().toString(), password.getText().toString(), vehicleInfo.getText().toString());
+            dao.add(acc).addOnSuccessListener(suc->{
+                Toast.makeText(this,"Account Created!", Toast.LENGTH_SHORT).show();
+                openActivityMain();
+            }).addOnFailureListener(er->{
+                Toast.makeText(this,""+er.getMessage(), Toast.LENGTH_SHORT).show();
+            });
+        });
 
         cancelButton = (Button) findViewById(R.id.cancelButton);
         cancelButton.setOnClickListener(new View.OnClickListener(){
