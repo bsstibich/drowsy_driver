7f9296dd487911f42d1877e75c2208af3a82fec5
Clay Gifford <claygifford1@gmail.com>
2021-11-17 11:44:20 -0600

    initial commit

---
+++ app/src/main/java/com/example/drowsy_driver/camFunctionality.java
@@ -0,0 +1,14 @@
+package com.example.drowsy_driver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class camFunctionality extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cam_functionality);
+    }
+}
